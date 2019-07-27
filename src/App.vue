4557e920b6949f87257e0c1f91d2d15db63ed77d
unknown <aurthurmusendame@gmail.com>
2019-07-28 00:12:59 +0200

    Added mdbvue UI Kit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <main-navigation></main-navigation>
     <router-view />
   </div>
 </template>
+
+<script>
+import MainNavigation from '@/components/MainNavigation.vue'
+export default {
+  components: {
+    MainNavigation
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/MainNavigation.vue
@@ -0,0 +1,50 @@
+<template>
+  <mdb-navbar expand="large" dark color="indigo">
+    <mdb-navbar-brand href="#">
+      CHORES
+    </mdb-navbar-brand>
+    <mdb-navbar-toggler>
+      <mdb-navbar-nav right>
+        <mdb-nav-item to="/">Home</mdb-nav-item>
+        <mdb-nav-item to="/about">About</mdb-nav-item>
+        <mdb-dropdown tag="li" class="nav-item">
+          <mdb-dropdown-toggle tag="a" navLink color="indigo" slot="toggle" waves-fixed>Dropdown</mdb-dropdown-toggle>
+          <mdb-dropdown-menu>
+            <mdb-dropdown-item>Action</mdb-dropdown-item>
+            <mdb-dropdown-item>Another action</mdb-dropdown-item>
+            <mdb-dropdown-item>Something else here</mdb-dropdown-item>
+          </mdb-dropdown-menu>
+        </mdb-dropdown>
+      </mdb-navbar-nav>
+    </mdb-navbar-toggler>
+  </mdb-navbar>
+</template>
+
+<script>
+import { 
+  mdbNavbar, 
+  mdbNavItem, 
+  mdbNavbarNav, 
+  mdbNavbarToggler, 
+  mdbNavbarBrand, 
+  mdbDropdown,
+  mdbDropdownToggle,
+  mdbDropdownMenu,
+  mdbDropdownItem, } from 'mdbvue';
+
+export default {
+  name: 'app',
+  components: {
+    mdbNavbar,
+    mdbNavItem,
+    mdbNavbarNav,
+    mdbNavbarToggler,
+    mdbNavbarBrand,
+    mdbDropdown,
+    mdbDropdownToggle,
+    mdbDropdownMenu,
+    mdbDropdownItem,
+  }
+};
+</script>
+
