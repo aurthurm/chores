7767a06207b9bbe31b0871513a48a3ba4add29e8
unknown <aurthurmusendame@gmail.com>
2019-07-28 15:23:46 +0200

    Added MDB query pro assets

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import store from "./store";
 import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'mdbvue/build/css/mdb.css'
 
+// Using MDBPRO JQueryVersion CSS Only for elements not in MDBVue Free
+import './assets/MDB-PRO-4.6.1/css/bootstrap.min.css'
+import './assets/MDB-PRO-4.6.1/css/mdb.min.css'
+
 Vue.config.productionTip = false;
 
 new Vue({
